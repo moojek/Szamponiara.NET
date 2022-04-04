14756e0c0c7d2b2d942f55a87c9d58d5c011cb12
moojek <42188458+moojek@users.noreply.github.com>
2022-04-04 21:23:38 +0200

    Move work to remote

---
+++ Szamponiara.App/Pages/Check/Check.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Szamponiara.App.Pages.Check
+{
+    public class CheckModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Szamponiara.App/Pages/Check/Results.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Szamponiara.App.Pages.Check
+{
+    public class ResultsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
